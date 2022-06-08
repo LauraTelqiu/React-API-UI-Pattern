415233a0a5bdc17b091713880a9a653a840eb3ae
Laura Telqiu <laurat@Lauras-MacBook-Air.local>
2022-06-08 10:36:48 -0400

    media

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,24 +5,24 @@ export default class Home extends Component {
   render() {
     return (
       <div className="home">
-        
+
         <div>
           <h1>Check out these five fun cat facts</h1>
         </div>
-          
 
-        <img
+
+        <img className="catpic"
           alt="catpic"
           src={
             "https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2F0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
           }
         />
-      
-       
-        
-          
+
+
+
+
       </div>
-     
+
     );
   }
 }
